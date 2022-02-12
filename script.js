9616117d4c6ad55791c485fc6e467301ae9cdda2
Peter Koveshnikov <rjdgtn@gmail.com>
2022-02-12 21:56:06 +0000

    fix form

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    // collect form data
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    // test values
+    if (principal <= 0) {
+        alert("Enter a positive number")
+        document.getElementById("principal").focus()
+    } else {
+        // produce result
+        document.getElementById("result").innerHTML="\
+            If you deposit "+principal+",<br/>\
+            at an interest rate of "+rate+"%.<br/>\
+            You will receive an amount of "+interest+",<br/>\
+            in the year "+year+"<br/>\
+            "
+    }
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
         
